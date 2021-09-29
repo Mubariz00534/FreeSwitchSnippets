c2749fb33165baf8e0afea876bf15002a11e258c
Mubariz00534 <mubarizmirzayev77@gmail.com>
2021-09-29 17:04:54 +0400

    Update curlPostFormatCdrDatas.sh

--- curlPostFormatCdrDatas.sh
+++ curlPostFormatCdrDatas.sh
@@ -2,5 +2,5 @@ nfiles=*.json
 for file in $nfiles
 do
     echo -e "\n----$file----"
-    curl -X POST -H "Content-Type: application/json" -d "@$file" "http://172.16.22.1/xmlcurl/cdr"
+    curl -X POST -H "Content-Type: application/json" -d "@$file" "http://x.x.x.x/xmlcurl/cdr"
 done
